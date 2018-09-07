8c76ab38f0f2d64976370513a3748ddf7bad6998
PSIB0T <arvindonarayanan@gmail.com>
2018-09-07 07:10:07 +0530

    Add: initial stages of Iterative Deepening

---
+++ iterative_deepening/IDeepening.py
@@ -0,0 +1,76 @@
+from puzzle import Puzzle
+
+# class Tree:
+#     def __init__(self, content, left=None, right=None):
+#         self.content = content
+#         self.left = left
+#         self.right = right
+    
+#     def get_children(self):
+#         children = []
+#         if self.left != None:
+#             children.append(self.left)
+#         if self.right != None:
+#             children.append(self.right)
+#         return children
+
+# def dfs(src, target):
+#     if src.content == target:
+#         return True
+#     for node in src.get_children():
+#         if dfs(node, target) == True:
+#             return True
+#     return False
+
+# tree = Tree(43)
+# tree_left = Tree(69)
+# tree_right = Tree(24)
+# tree.left = tree_left
+# tree23 = Tree(23)
+# tree.right = tree_right
+# # tree_left.left = tree23
+
+# print(dfs(tree, 23))
+
+class IterativeDeepening:
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
+        print(self.puzzle.state)
+
+    def dfs(self, src, target, limit, state_list):
+        print("New state")
+        print(src.puzzle)
+        if src.state == target:
+            return True, src
+        if limit <= 0:
+            return False, None   
+        print(state_list)
+        for move in src.get_moves():
+            if move.state not in state_list:
+                state_list.append(move.state)
+                isFinal, source = self.dfs(move, target, limit - 1, state_list)
+                if isFinal == True:
+                    return True, source
+        return False, None
+
+    def idfs(self, max_depth=20):
+        for i in range(max_depth):
+            print("Limit =", i)
+            state_list = []
+            state_list.append(self.puzzle.state)
+            isFinal, source = self.dfs(self.puzzle, self.puzzle.target_state, i, state_list)
+            if isFinal == True:
+                return i, source
+        return max_depth, None
+
+puzzle = Puzzle(2)
+print(puzzle.puzzle)
+print("Target state is ", puzzle.target_state)
+id = IterativeDeepening(puzzle)
+
+depth, source = id.idfs()
+print("Depth is", depth)
+if source != None:
+    print(source.puzzle)
+else:
+    print("No solution found")
--- iterative_deepening/puzzle.py
+++ iterative_deepening/puzzle.py
@@ -1,11 +1,12 @@
 import numpy as np
+import copy
 
 class Puzzle:
 
     # Constants that define puzzle movements
     UP = 0
-    DOWN = 1
-    LEFT = 2
+    DOWN = 2
+    LEFT = 1
     RIGHT = 3
 
     def __init__(self, size):
@@ -13,58 +14,62 @@ class Puzzle:
         self.puzzle = np.array([])
         self.blank = ()
         self.state = 0
+        self.target_state = 0
         self._generate_puzzle()
+
+    def get_moves(self):
+        # All legal moves in the puzzle
+        possible_moves = [self.UP, self.LEFT, self.DOWN, self.RIGHT]
+        # Moves that can be made in the current situation
+        right_moves = []
+        for move in possible_moves:
+            if self.move(move):
+                right_moves.append(copy.deepcopy(self))
+                self.move((move + 2) % 4)
+        return right_moves
     
     # Generates size X size puzzle
     def _generate_puzzle(self):
         self.state = 0
         self.puzzle = np.arange(self.size * self.size)
-        # np.random.shuffle(self.puzzle)
+        np.random.shuffle(self.puzzle)
         self.puzzle = self.puzzle.reshape(self.size, self.size)
         #  Stores the location of blank tile
         self.blank = tuple([x[0] for x in np.where(self.puzzle == 0)])
-        self._calc_state()
+        self.state = self._calc_state()
+        self.target_state = " ".join([str(i) for i in range(self.size ** 2)])
     
     def _calc_state(self):
-        i = 0
-        for row in self.puzzle:
-            for col in row:
-                self.state = self.state + col * i
-                i += 1
-        print(self.state)
+        return " ".join([str(i) for i in self.puzzle.ravel()])
     
     def _abs_cell_number(self, cell):
         return self.size * cell[0] + cell[1]
-    
-    def _update_state(self, blank_old):
-        arg_new = self.puzzle[blank_old]
-        print("New arg is " + str(arg_new))
-        self.state = self.state - self._abs_cell_number(self.blank) * arg_new + self._abs_cell_number(blank_old) * arg_new
 
     def move(self, direction):
         blank_old = self.blank
+        isPossible = False
         if direction == self.DOWN:
             if self.blank[0] != 0:
+                isPossible = True
                 self.puzzle[self.blank], self.puzzle[self.blank[0] - 1, self.blank[1]] = self.puzzle[self.blank[0] - 1, self.blank[1]], self.puzzle[self.blank]
                 self.blank = (self.blank[0] - 1, self.blank[1])
 
         if direction == self.UP:
             if self.blank[0] != self.size - 1:
+                isPossible = True
                 self.puzzle[self.blank], self.puzzle[self.blank[0] + 1, self.blank[1]] = self.puzzle[self.blank[0] + 1, self.blank[1]], self.puzzle[self.blank]
                 self.blank = (self.blank[0] + 1, self.blank[1])
 
         if direction == self.LEFT:
             if self.blank[1] != self.size - 1:
+                isPossible = True
                 self.puzzle[self.blank], self.puzzle[self.blank[0], self.blank[1] + 1] = self.puzzle[self.blank[0], self.blank[1] + 1], self.puzzle[self.blank]
                 self.blank = (self.blank[0], self.blank[1] + 1)
 
         if direction == self.RIGHT:
             if self.blank[1] != 0:
+                isPossible = True
                 self.puzzle[self.blank], self.puzzle[self.blank[0], self.blank[1] - 1] = self.puzzle[self.blank[0], self.blank[1] - 1], self.puzzle[self.blank]
                 self.blank = (self.blank[0], self.blank[1] - 1)
-        self._update_state(blank_old)
-
-
-if __name__ == "__main__":
-    p = Puzzle(2)
-    
+        self.state = self._calc_state()
+        return isPossible
